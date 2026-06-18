cf894f78ac2a4ce465b0992ff371324f09929ea1
InnoDev69 <yamirnu14@gmail.com>
2026-06-17 23:29:31 -0300

    Cambios
    -Se agrego el estilo a todos los inputs ya que no tenian ninguno.
    -Se mejoro el funcionamiento de auth_utils.
    -Se mejoro el diseño del file-filters (filtros de archivos) evitando usar el color blanco que no dejaba ver bien el texto.

--- src/auth_utils.py
+++ src/auth_utils.py
@@ -6,6 +6,6 @@ def login_required(func):
     @contextlib.wraps(func)
     def wrapper(*args, **kwargs):
         if not session.get("username"):
-            return render_template("login.html")
+            return render_template("login.html", error = "Debe iniciar sesión para acceder a esta página.")
         return func(*args, **kwargs)
     return wrapper
